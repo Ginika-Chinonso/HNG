0153f2b3a4a9b1d9831646a3341b86a1cc10aebc
rephanfrancis <ginika.chinonso.francis@gmail.com>
2022-10-31 02:14:29 +0100

    chore: Remove unwanted template code

--- src/components/profileimg.jsx
+++
@@ -1,8 +0,0 @@
-import profileimage from "../logo.svg"
-function ProfileImg () {
-    return (
-        <img src={profileimage} alt="Profile Image" srcset="" className="Profile-Image"/>
-    )
-}
-
-export default ProfileImg
